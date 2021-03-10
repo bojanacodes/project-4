23d4be86af7a31b4435f01c22f8d51bf5952479f
Eva Novotna <novotnae@gmail.com>
2021-03-10 22:49:44 +0100

    new models

--- server/models/link.py
+++ server/models/link.py
@@ -1,9 +1,7 @@
 
 from app import db
 from models.base import BaseModel
-# from models.cakes_ingredient import cakes_ingredients_join
 # ! Import ingredient into my cake so SQLAlchemy knows about it.
-# from models.ingredient import Ingredient
 from models.comment import Comment
 
 
@@ -23,7 +21,3 @@ class Link(db.Model, BaseModel):
         # ! Add the cascade keyword with all and delete.
     comments = db.relationship('Comment', backref='link', cascade="all, delete")
 
-    #     # ! Calling the backref cakes, because many ingredients to many cakes
-    #     # ! The extra keyword for M - M is the seconary keyword
-    #     # ? This secondary key will let us specify the JOIN table that relates the two models together.
-    #     ingredients = db.relationship('Ingredient', backref='cakes', secondary=cakes_ingredients_join
