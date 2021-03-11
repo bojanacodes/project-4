e23db94cfc8e7b040f1ad73acff1c88de62566a0
Bojana Sarenac <bojana.sarenac@gmail.com>
2021-03-11 11:28:39 +0000

    links set up

--- server/models/comment.py
+++ server/models/comment.py
@@ -1,13 +1,15 @@
-from app import db
-from models.base import BaseModel
-from models.user import User
+# from app import db
+# from models.base import BaseModel
+# from models.user import User
+# from datetime import datetime
 
-class Comment(db.Model, BaseModel):
+# class Comment(db.Model, BaseModel):
 
-    __tablename__ = 'comments'
-    content = db.Column(db.Text, nullable=False)
+#     __tablename__ = 'comments'
+#     content = db.Column(db.Text, nullable=False)
 
-    link_id = db.Column(db.Integer, db.ForeignKey('link.id', ondelete="CASCADE"))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="CASCADE"))
+#     link_id = db.Column(db.Integer, db.ForeignKey('links.id', ondelete="CASCADE"))
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="CASCADE"))
+#     edited_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    # ! we might need to add date edited here
+    
--- server/models/link.py
+++ server/models/link.py
@@ -2,7 +2,7 @@
 from app import db
 from models.base import BaseModel
 # ! Import ingredient into my cake so SQLAlchemy knows about it.
-from models.comment import Comment
+#from models.comment import Comment
 
 
 
@@ -10,18 +10,18 @@ from models.comment import Comment
 class Link(db.Model, BaseModel):
     __tablename__ = "links"
     name = db.Column(db.String(40), nullable=False)
-    description = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=True)
     url = db.Column(db.Text, nullable=False)
-    image = db.Column(db.Text, nullable=False)
+    image = db.Column(db.Text, nullable=True)
     importance = db.Column(db.String(40), nullable=False)
 
 
     # Foreign keys
-    folder_id = db.Column(db.Integer, db.ForeignKey('folder.id', ondelete="CASCADE"))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="CASCADE"))
+    #folder_id = db.Column(db.Integer, db.ForeignKey('folders.id', ondelete="CASCADE"))
+   # user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="CASCADE"))
 
 
     # # * FOR CASCADING DELETION:
         # ! Add the cascade keyword with all and delete.
-    comments = db.relationship('Comment', backref='link', cascade="all, delete")
+    #comments = db.relationship('Comment', backref='link', cascade="all, delete")
 
