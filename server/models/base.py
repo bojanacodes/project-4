45f99b5874281d08ad478ff3106ff77d13b3ce32
Eva Novotna <novotnae@gmail.com>
2021-03-15 12:42:37 +0100

    edited comments model

--- server/models/base.py
+++ server/models/base.py
@@ -6,6 +6,8 @@ from app import db
 class BaseModel:
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    # updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
 
     # ! Adding a method in here to save a line of code in saving.
     def save(self):
