1eb4de24a1813e888a07d427b62d4768fa69ce08
Bojana Sarenac <bojana.sarenac@gmail.com>
2021-11-06 20:57:39 +0000

    Ready for deployment

--- server/config/environment.py
+++ server/config/environment.py
@@ -1,4 +1,4 @@
 import os
 
 db_URI = os.getenv('DATABASE_URL', 'postgres://localhost:5432/project4_db')
-secret = os.getenv('SECRET', 'newspaper dolphin unicorn curtains expedition flowers necklace')
+secret = os.getenv('SECRET', 'green marmalade queen pelican rocket range')
